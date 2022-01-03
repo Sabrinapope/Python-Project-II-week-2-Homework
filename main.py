92c82f77491e8084c722243183f2d48a55c305bc
Sabrinapope.md <40004402+Sabrinapope@users.noreply.github.com>
2022-01-02 19:26:25 -0500

    still in progress

---
+++ main.py
@@ -0,0 +1,41 @@
+mystr = "TRUE"
+for true in mystr:
+    print ('TRUE')
+    continue
+
+numbers = [2, 3, 10]
+for numbers in range(2, 10):
+   print(numbers)
+
+
+chore_list = ["Laundry", "mop floors", "clean bathroom", "clean fridge", "vacuum", "dusting", "cooking", "grocery shop", "car wash" , "pay bills"]
+chore_list.remove("pay bills")
+print(chore_list)
+print(True)
+
+
+input = str('Python')
+print('the str is not and integer')
+
+
+numbers_list1 = ["111, 32, -9, -45, -17, 9, -10"]
+numbers_list2 = ["110, 32, 9, "]
+for i in range(111, -10):
+    print(numbers_lists.append(i)
+    
+    
+user_num = input("1,2,3,4,5"):
+user_num = sum(user_num)
+print(sum)
+
+user_num = 7
+
+primes = []
+
+for i in range(2, user_num + 1):
+    if i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            primes.append(i)
